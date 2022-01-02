719df50a802f62209d9bbe7a93bd22b365fd8516
sastubblefield <stubblefield.sa@gmail.com>
2022-01-02 09:45:15 -0800

    Set Up & API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1 className="heading font-weight-lighter">
         What would you like to search for today?
       </h1>
-      <h2 className="mt-5">DICTIONARY SEARCH</h2>
+      <h2 className="mt-5 font-weight-normal">DICTIONARY SEARCH</h2>
       <Dictionary />
     </div>
   );
