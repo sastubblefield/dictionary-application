e7e42376207008bc01abbfab615b2acad87c0bb0
sastubblefield <stubblefield.sa@gmail.com>
2022-01-16 12:10:35 -0800

    open source link

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <h1 className="heading font-weight-lighter">
+      <h1 className="heading font-weight-lighter ">
         What would you like to search for today?
       </h1>
       <h2 className="mt-5 font-weight-normal">DICTIONARY SEARCH</h2>
       <Dictionary />
+      <div className="font-weight-lighter text-center">
+        <div>
+          <a
+          className="open-source-link"
+            href="https://github.com/sastubblefield/dictionary-application"
+            target="blank"
+          >
+            Open Source Code
+          </a>
+        </div>
+        By Shannon Stubblefield
+      </div>
     </div>
   );
 }
